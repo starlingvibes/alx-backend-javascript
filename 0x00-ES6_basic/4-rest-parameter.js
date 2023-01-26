8e39877e0403ee8396d50fabebde44c7f6738963
starlingvibes <starlingroot@protonmail.com>
2023-01-26 16:01:06 +0100

    count number of arguments

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,7 @@
 export default function returnHowManyArguments(...theArgs) {
   let total = 0;
   for (let arg of theArgs) {
+    // eslint-disable-line no-unused-vars
     total += 1;
   }
   return total;
