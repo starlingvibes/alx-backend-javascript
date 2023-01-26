6b71333f85ff62a8f53360d87f6f2b571e964c5a
starlingvibes <starlingroot@protonmail.com>
2023-01-26 15:11:23 +0100

    used const for the variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  // var task = false;
-  // var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  } else {
-    return [false, true];
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];
