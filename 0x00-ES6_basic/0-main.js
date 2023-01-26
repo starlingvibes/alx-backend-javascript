6e9f8743108dbf9c97cf196558aff951aef6b059
starlingvibes <starlingroot@protonmail.com>
2023-01-26 15:37:27 +0100

    using default parameters

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,6 +1,7 @@
 import { taskFirst, taskNext } from './0-constants.js';
 import taskBlock from './1-block-scoped.js';
 import getNeighborhoodsList from './2-arrow.js';
+import getSumOfHoods from './3-default-parameter.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
 console.log(taskBlock(true));
@@ -8,3 +9,6 @@ console.log(taskBlock(false));
 const neighborhoodsList = new getNeighborhoodsList();
 const res = neighborhoodsList.addNeighborhood('Noe Valley');
 console.log(res);
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
