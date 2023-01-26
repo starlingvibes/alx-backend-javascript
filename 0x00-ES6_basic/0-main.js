e2352f93b52b6da93f456004b7b6ef14ec148de9
starlingvibes <starlingroot@protonmail.com>
2023-01-26 15:30:55 +0100

    rewriting class to one with arrow functions

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,6 +1,10 @@
 import { taskFirst, taskNext } from './0-constants.js';
 import taskBlock from './1-block-scoped.js';
+import getNeighborhoodsList from './2-arrow.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
 console.log(taskBlock(true));
 console.log(taskBlock(false));
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
