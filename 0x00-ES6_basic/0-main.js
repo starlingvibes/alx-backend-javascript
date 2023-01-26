3253099c53e8c7668499efeac974571afcf00d21
starlingvibes <starlingroot@protonmail.com>
2023-01-26 17:13:30 +0100

    ES6 method properties

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -7,6 +7,7 @@ import concatArrays from './5-spread-operator.js';
 import getSanFranciscoDescription from './6-string-interpolation.js';
 import getBudgetObject from './7-getBudgetObject.js';
 import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+import getFullBudgetObject from './9-getFullBudget.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
 console.log(taskBlock(true));
@@ -22,3 +23,7 @@ console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
 console.log(getSanFranciscoDescription());
 console.log(getBudgetObject(400, 700, 900));
 console.log(getBudgetForCurrentYear(2100, 5200, 1090));
+const fullBudget = getFullBudgetObject(20, 50, 10);
+
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
