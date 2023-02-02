5ad555e697689829277ffa084742b0dca3a1b48f
starlingvibes <starlingroot@protonmail.com>
2023-02-02 12:57:31 +0100

    printing teachers

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,10 +1,8 @@
-"use strict";
+'use strict';
 exports.__esModule = true;
-var teacher3 = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false
-};
-console.log(teacher3);
+exports.printTeacher = void 0;
+function printTeacher(firstName, lastName) {
+  return ''.concat(firstName.charAt(0), '. ').concat(lastName);
+}
+exports.printTeacher = printTeacher;
+console.log(printTeacher('Chidera', 'Anichebe'));
