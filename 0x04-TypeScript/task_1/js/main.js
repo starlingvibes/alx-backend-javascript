b32fd949a071dab941c3ad98a12903f7a66c0a56
starlingvibes <starlingroot@protonmail.com>
2023-02-02 12:44:28 +0100

    building a teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false
+};
+console.log(teacher3);
