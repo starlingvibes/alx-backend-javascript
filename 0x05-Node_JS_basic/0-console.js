3006f93123970613ed9f97e23aeea87cd6ad7857
starlingvibes <starlingroot@protonmail.com>
2023-04-07 19:24:43 +0100

    console js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(message) {
-  console.log(message);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
