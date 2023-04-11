6d59a58e6d30666051a704070c17c41f3c96ee10
starlingvibes <starlingroot@protonmail.com>
2023-04-11 19:19:29 +0100

    initial commit

--- 0x01-ES6_promise/0-main.js
+++
@@ -1,7 +0,0 @@
-import getResponseFromAPI from './0-promise.js';
-import getFullResponseFromAPI from './1-promise.js';
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
--- README.md
+++
@@ -1,2 +0,0 @@
-# alx-backend-javascript
-3 months Backend specialization for the ALX Software Engineering programme 
